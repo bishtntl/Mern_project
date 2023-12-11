93ced5fff41a43177e6f12481f8dd357d497c954
bishtntl <bishtntl04@gmail.com>
2023-12-11 15:47:28 +0530

    re

--- src/Component/Private.js
+++ src/Component/Private.js
@@ -3,6 +3,6 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateCompo =()=>{
   
     const auth=localStorage.getItem("token")
-    return auth ? <Outlet/> : <Navigate to="/register"/>
+    return auth ? <Outlet/> : <Navigate to="register"/>
 }
 export default PrivateCompo;
